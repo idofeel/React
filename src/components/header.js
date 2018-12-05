bb2127cd190c048bf35c4a013d88b63e2e30c3b1
杜飞 <dufei@dufeideMacBook-Pro.local>
2018-12-05 19:17:04 +0800

    测试git合并

--- src/components/header.js
+++ src/components/header.js
@@ -3,28 +3,27 @@ import React, { Component } from "react";
 export default class Header extends Component {
   constructor(props) {
     super(props);
+    // 父组件传入的参数 props
     console.log(props);
+    // react 特性 >> state属性改变会发生重新渲染
     this.state = {
-      age: props.age
+      name: props.name
     };
   }
   addAge() {
-    // react 特性 >> state属性改变会发生重新渲染
     // 通过setState方法进行改变数据
     this.setState({
-      age: this.state.age + 3
+      name: this.state.name + 3
     });
   }
-
   sendParent(){
-      this.props.callback(this.state.age);
+      this.props.callback(this.state.name);
   }
-  
   render() {
     return (
       <div>
         <h1 onClick={()=>{this.sendParent()}}>Header</h1>
-        <span onClick={this.addAge.bind(this)}>{this.state.age}</span>
+        <span onClick={this.addAge.bind(this)}>{this.state.name}</span>
       </div>
     );
   }
